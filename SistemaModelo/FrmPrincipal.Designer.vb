a165750e0b4ebc357c5d7569a16f1a33f1b58fdb
Rodrigo Fernando Gomes Lima <rfergomes@gmail.com>
2022-01-21 21:57:27 -0300

    Atualizações

--- SistemaModelo/FrmPrincipal.Designer.vb
+++ SistemaModelo/FrmPrincipal.Designer.vb
@@ -24,29 +24,33 @@ Partial Class FrmPrincipal
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmPrincipal))
         Me.PanelMenu = New System.Windows.Forms.Panel()
-        Me.BtMenu1 = New FontAwesome.Sharp.IconButton()
+        Me.IconButton5 = New FontAwesome.Sharp.IconButton()
+        Me.IconButton4 = New FontAwesome.Sharp.IconButton()
+        Me.IconButton3 = New FontAwesome.Sharp.IconButton()
+        Me.IconButton2 = New FontAwesome.Sharp.IconButton()
+        Me.IconButton1 = New FontAwesome.Sharp.IconButton()
         Me.PanelLogo = New System.Windows.Forms.Panel()
         Me.LabelLogo = New System.Windows.Forms.Label()
         Me.PictureBoxLogo = New System.Windows.Forms.PictureBox()
         Me.PanelTopo = New System.Windows.Forms.Panel()
         Me.PanelInfo = New System.Windows.Forms.Panel()
-        Me.IconButton1 = New FontAwesome.Sharp.IconButton()
-        Me.IconButton2 = New FontAwesome.Sharp.IconButton()
-        Me.IconButton3 = New FontAwesome.Sharp.IconButton()
-        Me.IconButton4 = New FontAwesome.Sharp.IconButton()
+        Me.LabelTitulo = New System.Windows.Forms.Label()
+        Me.IconTitulo = New FontAwesome.Sharp.IconPictureBox()
         Me.PanelMenu.SuspendLayout()
         Me.PanelLogo.SuspendLayout()
         CType(Me.PictureBoxLogo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelInfo.SuspendLayout()
+        CType(Me.IconTitulo, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PanelMenu
         '
         Me.PanelMenu.BackColor = System.Drawing.Color.FromArgb(CType(CType(52, Byte), Integer), CType(CType(58, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.PanelMenu.Controls.Add(Me.IconButton5)
         Me.PanelMenu.Controls.Add(Me.IconButton4)
         Me.PanelMenu.Controls.Add(Me.IconButton3)
         Me.PanelMenu.Controls.Add(Me.IconButton2)
         Me.PanelMenu.Controls.Add(Me.IconButton1)
-        Me.PanelMenu.Controls.Add(Me.BtMenu1)
         Me.PanelMenu.Controls.Add(Me.PanelLogo)
         Me.PanelMenu.Dock = System.Windows.Forms.DockStyle.Left
         Me.PanelMenu.Location = New System.Drawing.Point(0, 0)
@@ -54,27 +58,115 @@ Partial Class FrmPrincipal
         Me.PanelMenu.Size = New System.Drawing.Size(200, 681)
         Me.PanelMenu.TabIndex = 0
         '
-        'BtMenu1
+        'IconButton5
         '
-        Me.BtMenu1.Dock = System.Windows.Forms.DockStyle.Top
-        Me.BtMenu1.FlatAppearance.BorderSize = 0
-        Me.BtMenu1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.BtMenu1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.BtMenu1.ForeColor = System.Drawing.Color.Gainsboro
-        Me.BtMenu1.IconChar = FontAwesome.Sharp.IconChar.TachometerAlt
-        Me.BtMenu1.IconColor = System.Drawing.Color.Gainsboro
-        Me.BtMenu1.IconFont = FontAwesome.Sharp.IconFont.[Auto]
-        Me.BtMenu1.IconSize = 32
-        Me.BtMenu1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.BtMenu1.Location = New System.Drawing.Point(0, 100)
-        Me.BtMenu1.Name = "BtMenu1"
-        Me.BtMenu1.Padding = New System.Windows.Forms.Padding(10, 0, 20, 0)
-        Me.BtMenu1.Size = New System.Drawing.Size(200, 60)
-        Me.BtMenu1.TabIndex = 2
-        Me.BtMenu1.Text = "DASHBOARD"
-        Me.BtMenu1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.BtMenu1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
-        Me.BtMenu1.UseVisualStyleBackColor = True
+        Me.IconButton5.Dock = System.Windows.Forms.DockStyle.Top
+        Me.IconButton5.FlatAppearance.BorderSize = 0
+        Me.IconButton5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButton5.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.IconButton5.ForeColor = System.Drawing.Color.Gainsboro
+        Me.IconButton5.IconChar = FontAwesome.Sharp.IconChar.DoorOpen
+        Me.IconButton5.IconColor = System.Drawing.Color.Gainsboro
+        Me.IconButton5.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton5.IconSize = 32
+        Me.IconButton5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton5.Location = New System.Drawing.Point(0, 340)
+        Me.IconButton5.Name = "IconButton5"
+        Me.IconButton5.Padding = New System.Windows.Forms.Padding(10, 0, 20, 0)
+        Me.IconButton5.Size = New System.Drawing.Size(200, 60)
+        Me.IconButton5.TabIndex = 6
+        Me.IconButton5.Text = "SAIR"
+        Me.IconButton5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.IconButton5.UseVisualStyleBackColor = True
+        '
+        'IconButton4
+        '
+        Me.IconButton4.Dock = System.Windows.Forms.DockStyle.Top
+        Me.IconButton4.FlatAppearance.BorderSize = 0
+        Me.IconButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButton4.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.IconButton4.ForeColor = System.Drawing.Color.Gainsboro
+        Me.IconButton4.IconChar = FontAwesome.Sharp.IconChar.Child
+        Me.IconButton4.IconColor = System.Drawing.Color.Gainsboro
+        Me.IconButton4.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton4.IconSize = 32
+        Me.IconButton4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton4.Location = New System.Drawing.Point(0, 280)
+        Me.IconButton4.Name = "IconButton4"
+        Me.IconButton4.Padding = New System.Windows.Forms.Padding(10, 0, 20, 0)
+        Me.IconButton4.Size = New System.Drawing.Size(200, 60)
+        Me.IconButton4.TabIndex = 5
+        Me.IconButton4.Text = "PÁGINA 3"
+        Me.IconButton4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.IconButton4.UseVisualStyleBackColor = True
+        '
+        'IconButton3
+        '
+        Me.IconButton3.Dock = System.Windows.Forms.DockStyle.Top
+        Me.IconButton3.FlatAppearance.BorderSize = 0
+        Me.IconButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButton3.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.IconButton3.ForeColor = System.Drawing.Color.Gainsboro
+        Me.IconButton3.IconChar = FontAwesome.Sharp.IconChar.ChartBar
+        Me.IconButton3.IconColor = System.Drawing.Color.Gainsboro
+        Me.IconButton3.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton3.IconSize = 32
+        Me.IconButton3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton3.Location = New System.Drawing.Point(0, 220)
+        Me.IconButton3.Name = "IconButton3"
+        Me.IconButton3.Padding = New System.Windows.Forms.Padding(10, 0, 20, 0)
+        Me.IconButton3.Size = New System.Drawing.Size(200, 60)
+        Me.IconButton3.TabIndex = 4
+        Me.IconButton3.Text = "PÁGINA 2"
+        Me.IconButton3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.IconButton3.UseVisualStyleBackColor = True
+        '
+        'IconButton2
+        '
+        Me.IconButton2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.IconButton2.FlatAppearance.BorderSize = 0
+        Me.IconButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButton2.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.IconButton2.ForeColor = System.Drawing.Color.Gainsboro
+        Me.IconButton2.IconChar = FontAwesome.Sharp.IconChar.AddressBook
+        Me.IconButton2.IconColor = System.Drawing.Color.Gainsboro
+        Me.IconButton2.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton2.IconSize = 32
+        Me.IconButton2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton2.Location = New System.Drawing.Point(0, 160)
+        Me.IconButton2.Name = "IconButton2"
+        Me.IconButton2.Padding = New System.Windows.Forms.Padding(10, 0, 20, 0)
+        Me.IconButton2.Size = New System.Drawing.Size(200, 60)
+        Me.IconButton2.TabIndex = 3
+        Me.IconButton2.Text = "PÁGINA 1"
+        Me.IconButton2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.IconButton2.UseVisualStyleBackColor = True
+        '
+        'IconButton1
+        '
+        Me.IconButton1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.IconButton1.FlatAppearance.BorderSize = 0
+        Me.IconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.IconButton1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.IconButton1.ForeColor = System.Drawing.Color.Gainsboro
+        Me.IconButton1.IconChar = FontAwesome.Sharp.IconChar.TachometerAlt
+        Me.IconButton1.IconColor = System.Drawing.Color.Gainsboro
+        Me.IconButton1.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton1.IconSize = 32
+        Me.IconButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton1.Location = New System.Drawing.Point(0, 100)
+        Me.IconButton1.Name = "IconButton1"
+        Me.IconButton1.Padding = New System.Windows.Forms.Padding(10, 0, 20, 0)
+        Me.IconButton1.Size = New System.Drawing.Size(200, 60)
+        Me.IconButton1.TabIndex = 2
+        Me.IconButton1.Text = "DASHBOARD"
+        Me.IconButton1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.IconButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.IconButton1.UseVisualStyleBackColor = True
         '
         'PanelLogo
         '
@@ -121,99 +213,36 @@ Partial Class FrmPrincipal
         'PanelInfo
         '
         Me.PanelInfo.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.PanelInfo.Controls.Add(Me.LabelTitulo)
+        Me.PanelInfo.Controls.Add(Me.IconTitulo)
         Me.PanelInfo.Dock = System.Windows.Forms.DockStyle.Top
         Me.PanelInfo.Location = New System.Drawing.Point(200, 65)
         Me.PanelInfo.Name = "PanelInfo"
         Me.PanelInfo.Size = New System.Drawing.Size(1064, 35)
         Me.PanelInfo.TabIndex = 2
         '
-        'IconButton1
+        'LabelTitulo
         '
-        Me.IconButton1.Dock = System.Windows.Forms.DockStyle.Top
-        Me.IconButton1.FlatAppearance.BorderSize = 0
-        Me.IconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.IconButton1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.IconButton1.ForeColor = System.Drawing.Color.Gainsboro
-        Me.IconButton1.IconChar = FontAwesome.Sharp.IconChar.AddressBook
-        Me.IconButton1.IconColor = System.Drawing.Color.Gainsboro
-        Me.IconButton1.IconFont = FontAwesome.Sharp.IconFont.[Auto]
-        Me.IconButton1.IconSize = 32
-        Me.IconButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.IconButton1.Location = New System.Drawing.Point(0, 160)
-        Me.IconButton1.Name = "IconButton1"
-        Me.IconButton1.Padding = New System.Windows.Forms.Padding(10, 0, 20, 0)
-        Me.IconButton1.Size = New System.Drawing.Size(200, 60)
-        Me.IconButton1.TabIndex = 3
-        Me.IconButton1.Text = "PÁGINA 1"
-        Me.IconButton1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.IconButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
-        Me.IconButton1.UseVisualStyleBackColor = True
+        Me.LabelTitulo.AutoSize = True
+        Me.LabelTitulo.ForeColor = System.Drawing.SystemColors.ButtonShadow
+        Me.LabelTitulo.Location = New System.Drawing.Point(44, 11)
+        Me.LabelTitulo.Name = "LabelTitulo"
+        Me.LabelTitulo.Size = New System.Drawing.Size(77, 15)
+        Me.LabelTitulo.TabIndex = 3
+        Me.LabelTitulo.Text = "DASHBOARD"
         '
-        'IconButton2
+        'IconTitulo
         '
-        Me.IconButton2.Dock = System.Windows.Forms.DockStyle.Top
-        Me.IconButton2.FlatAppearance.BorderSize = 0
-        Me.IconButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.IconButton2.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.IconButton2.ForeColor = System.Drawing.Color.Gainsboro
-        Me.IconButton2.IconChar = FontAwesome.Sharp.IconChar.ChartBar
-        Me.IconButton2.IconColor = System.Drawing.Color.Gainsboro
-        Me.IconButton2.IconFont = FontAwesome.Sharp.IconFont.[Auto]
-        Me.IconButton2.IconSize = 32
-        Me.IconButton2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.IconButton2.Location = New System.Drawing.Point(0, 220)
-        Me.IconButton2.Name = "IconButton2"
-        Me.IconButton2.Padding = New System.Windows.Forms.Padding(10, 0, 20, 0)
-        Me.IconButton2.Size = New System.Drawing.Size(200, 60)
-        Me.IconButton2.TabIndex = 4
-        Me.IconButton2.Text = "PÁGINA 2"
-        Me.IconButton2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.IconButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
-        Me.IconButton2.UseVisualStyleBackColor = True
-        '
-        'IconButton3
-        '
-        Me.IconButton3.Dock = System.Windows.Forms.DockStyle.Top
-        Me.IconButton3.FlatAppearance.BorderSize = 0
-        Me.IconButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.IconButton3.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.IconButton3.ForeColor = System.Drawing.Color.Gainsboro
-        Me.IconButton3.IconChar = FontAwesome.Sharp.IconChar.Child
-        Me.IconButton3.IconColor = System.Drawing.Color.Gainsboro
-        Me.IconButton3.IconFont = FontAwesome.Sharp.IconFont.[Auto]
-        Me.IconButton3.IconSize = 32
-        Me.IconButton3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.IconButton3.Location = New System.Drawing.Point(0, 280)
-        Me.IconButton3.Name = "IconButton3"
-        Me.IconButton3.Padding = New System.Windows.Forms.Padding(10, 0, 20, 0)
-        Me.IconButton3.Size = New System.Drawing.Size(200, 60)
-        Me.IconButton3.TabIndex = 5
-        Me.IconButton3.Text = "PÁGINA 3"
-        Me.IconButton3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.IconButton3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
-        Me.IconButton3.UseVisualStyleBackColor = True
-        '
-        'IconButton4
-        '
-        Me.IconButton4.Dock = System.Windows.Forms.DockStyle.Top
-        Me.IconButton4.FlatAppearance.BorderSize = 0
-        Me.IconButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.IconButton4.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.IconButton4.ForeColor = System.Drawing.Color.Gainsboro
-        Me.IconButton4.IconChar = FontAwesome.Sharp.IconChar.Church
-        Me.IconButton4.IconColor = System.Drawing.Color.Gainsboro
-        Me.IconButton4.IconFont = FontAwesome.Sharp.IconFont.[Auto]
-        Me.IconButton4.IconSize = 32
-        Me.IconButton4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.IconButton4.Location = New System.Drawing.Point(0, 340)
-        Me.IconButton4.Name = "IconButton4"
-        Me.IconButton4.Padding = New System.Windows.Forms.Padding(10, 0, 20, 0)
-        Me.IconButton4.Size = New System.Drawing.Size(200, 60)
-        Me.IconButton4.TabIndex = 6
-        Me.IconButton4.Text = "PÁGINA 4"
-        Me.IconButton4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.IconButton4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
-        Me.IconButton4.UseVisualStyleBackColor = True
+        Me.IconTitulo.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.IconTitulo.ForeColor = System.Drawing.SystemColors.ButtonShadow
+        Me.IconTitulo.IconChar = FontAwesome.Sharp.IconChar.Home
+        Me.IconTitulo.IconColor = System.Drawing.SystemColors.ButtonShadow
+        Me.IconTitulo.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconTitulo.Location = New System.Drawing.Point(6, 1)
+        Me.IconTitulo.Name = "IconTitulo"
+        Me.IconTitulo.Size = New System.Drawing.Size(32, 32)
+        Me.IconTitulo.TabIndex = 0
+        Me.IconTitulo.TabStop = False
         '
         'FrmPrincipal
         '
@@ -230,6 +259,9 @@ Partial Class FrmPrincipal
         Me.PanelMenu.ResumeLayout(False)
         Me.PanelLogo.ResumeLayout(False)
         CType(Me.PictureBoxLogo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelInfo.ResumeLayout(False)
+        Me.PanelInfo.PerformLayout()
+        CType(Me.IconTitulo, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -240,9 +272,11 @@ Partial Class FrmPrincipal
     Friend WithEvents PictureBoxLogo As PictureBox
     Friend WithEvents PanelTopo As Panel
     Friend WithEvents PanelInfo As Panel
-    Friend WithEvents BtMenu1 As FontAwesome.Sharp.IconButton
+    Friend WithEvents IconButton1 As FontAwesome.Sharp.IconButton
+    Friend WithEvents IconButton5 As FontAwesome.Sharp.IconButton
     Friend WithEvents IconButton4 As FontAwesome.Sharp.IconButton
     Friend WithEvents IconButton3 As FontAwesome.Sharp.IconButton
     Friend WithEvents IconButton2 As FontAwesome.Sharp.IconButton
-    Friend WithEvents IconButton1 As FontAwesome.Sharp.IconButton
+    Friend WithEvents LabelTitulo As Label
+    Friend WithEvents IconTitulo As FontAwesome.Sharp.IconPictureBox
 End Class
--- SistemaModelo/FrmPrincipal.vb
+++ SistemaModelo/FrmPrincipal.vb
@@ -1,3 +1,75 @@
-﻿Public Class FrmPrincipal
+﻿Imports FontAwesome.Sharp
+Public Class FrmPrincipal
+    'Campos
+    Private currentBtn As IconButton
+    Private ReadOnly leftBorderBtn As Panel
+    'Construtor
+    Public Sub New()
+        'Chamada requerida para o designer
+        InitializeComponent()
+        'Adiciona qualquer inicialização após a chamada InitializeComponent().
+        leftBorderBtn = New Panel With {
+            .Size = New Size(7, 60)
+        }
+        PanelMenu.Controls.Add(leftBorderBtn)
+    End Sub
+    Private Sub ActiveButton(senderBtn As Object, customColor As Color)
+        If senderBtn IsNot Nothing Then
+            DisableButton()
+            'Button
+            currentBtn = CType(senderBtn, IconButton)
+            With currentBtn
+                '.BackColor = Color.FromArgb(52, 58, 64)
+                .ForeColor = customColor
+                .IconColor = customColor
+                .TextAlign = ContentAlignment.MiddleCenter
+                .ImageAlign = ContentAlignment.MiddleRight
+                .TextImageRelation = TextImageRelation.TextBeforeImage
+            End With
+            'Left Border
+            With leftBorderBtn
+                .BackColor = customColor
+                .Location = New Point(0, currentBtn.Location.Y)
+                .Visible = True
+                .BringToFront()
+            End With
+            IconTitulo.IconChar = currentBtn.IconChar
+            IconTitulo.IconColor = customColor
+        End If
+    End Sub
+    Private Sub DisableButton()
+        If currentBtn IsNot Nothing Then
+            With currentBtn
+                '.BackColor = Color.FromArgb(52, 58, 64)
+                .ForeColor = Color.Gainsboro
+                .IconColor = Color.Gainsboro
+                .TextAlign = ContentAlignment.MiddleLeft
+                .ImageAlign = ContentAlignment.MiddleLeft
+                .TextImageRelation = TextImageRelation.ImageBeforeText
+            End With
+        End If
+    End Sub
+    Private Sub FrmPrincipal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub IconButton1_Click(sender As Object, e As EventArgs) Handles IconButton1.Click
+        ActiveButton(sender, RGBColors.Cor1)
+    End Sub
+
+    Private Sub IconButton2_Click(sender As Object, e As EventArgs) Handles IconButton2.Click
+        ActiveButton(sender, RGBColors.Cor2)
+    End Sub
+
+    Private Sub IconButton3_Click(sender As Object, e As EventArgs) Handles IconButton3.Click
+        ActiveButton(sender, RGBColors.Cor3)
+    End Sub
+
+    Private Sub IconButton4_Click(sender As Object, e As EventArgs) Handles IconButton4.Click
+        ActiveButton(sender, RGBColors.Cor4)
+    End Sub
+
+    Private Sub IconButton5_Click(sender As Object, e As EventArgs) Handles IconButton5.Click
+        Application.Exit()
+    End Sub
 End Class
